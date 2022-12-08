547ada39aba033de1cc501cd81536ad5c4b4c6db
rizood <114640452+rizood@users.noreply.github.com>
2022-12-09 00:19:21 +0800

    Update menu.sh

--- SSH/menu.sh
+++ SSH/menu.sh
@@ -53,7 +53,7 @@ echo -e " IP VPS          :  $IPVPS"
 echo -e " Version         : \033[1;31m SC (V7)\e[0m"
 echo -e " Expiry Script   : \033[1;31m Lifetime\e[0m"
 echo -e " Client Name     :  $user"
-echo -e " Created By      :  @GHReyz"
+echo -e " Autoscript By   :  @GHReyz"
 echo -e "\e[35m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;47;30m               • XRAY MENU •                \E[0m"
 echo -e "\e[35m╘════════════════════════════════════════════╛\033[0m
