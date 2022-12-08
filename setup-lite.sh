502b9c0abe6d5c44a1097a85a781edf37bc98641
rizood <114640452+rizood@users.noreply.github.com>
2022-12-08 11:12:31 +0800

    Update setup-lite.sh

--- setup-lite.sh
+++ setup-lite.sh
@@ -25,7 +25,7 @@ echo -e "\e[0m"
 echo ""
 sleep 1
 # Insert Password
-Password=MultiV7Reyz
+Password=hariz
 # Execute
 read -p "Please Insert The License Key : " Passwordnya
 if [ $Password = $Passwordnya ]; then
